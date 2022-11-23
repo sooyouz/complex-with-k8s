1c155ba72b8587fe3164a97ac701b2d3fa801ac4
sooyouz <sooyouz@DESKTOP-0R8PTLC.localdomain>
2022-11-23 22:17:44 +0100

    adding gKE conf

---
+++ deploy.sh
@@ -0,0 +1,15 @@
+docker build -t sooyouz/multi-client:$GIT_SHA -t sooyouz/multi-client:latest -f ./client/Dockerfile ./client
+docker build -t sooyouz/multi-server:$GIT_SHA -t sooyouz/multi-server:latest -f ./server/Dockerfile ./server
+docker build -t sooyouz/multi-worker:$GIT_SHA -t sooyouz/multi-worker:latest -f ./worker/Dockerfile ./worker
+docker push sooyouz/multi-client:$GIT_SHA
+docker push sooyouz/multi-server:$GIT_SHA
+docker push sooyouz/multi-worker:$GIT_SHA
+docker push sooyouz/multi-client:latest
+docker push sooyouz/multi-server:latest
+docker push sooyouz/multi-worker:latest
+
+kubectl apply -f k8s
+
+kubectl set image deployments/server-deployments server=sooyouz/multi-server:$GIT_SHA
+kubectl set image deployments/client-deployments client=sooyouz/multi-client:$GIT_SHA
+kubectl set image deployments/worker-deployments worker=sooyouz/multi-worker:$GIT_SHA
